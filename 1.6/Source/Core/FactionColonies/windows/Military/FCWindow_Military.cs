829cb10776f330d096f3c4b5fe051b1e11a9112d
matathias <warrick.david58@gmail.com>
2026-06-08 23:51:06 -0700

    Mercs: widen the unit/squad designer window

--- 1.6/Source/Core/FactionColonies/windows/Military/FCWindow_Military.cs
+++ 1.6/Source/Core/FactionColonies/windows/Military/FCWindow_Military.cs
@@ -9,7 +9,7 @@ namespace FactionColonies
         private readonly MilitaryWindow militaryWindow;
         private readonly string title;
 
-        public override Vector2 InitialSize => new Vector2(950f, 530f);
+        public override Vector2 InitialSize => new Vector2(1050f, 600f);
 
         public FCWindow_Military(MilitaryWindow militaryWindow, string title)
         {
